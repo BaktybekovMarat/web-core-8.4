7e6264792414d0cb488083f9699a42ac95196630
Marat <mrtbaktybek@gmail.com>
2026-02-26 22:43:52 +0900

    update code

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let swiper;
 
 function initSwiper() {
-  if (window.innerWidth < 768 && !swiper) { //проверяем условие
+  if (window.innerWidth < 768 && !swiper) {
+    //проверяем условие
     swiper = new Swiper(".swiper", {
       centeredSlides: true, // центрировать активный слайд
       slidesPerView: 1, // показывать только один слайд
-      pagination: { //устанавливаем пагинацию
+      pagination: {
+        //устанавливаем пагинацию
         el: ".swiper-pagination",
         clickable: true, //пагинацию можно кликнуть и перейти на тот слайд
       },
     });
-  } else if (window.innerWidth >= 768 && swiper) { //если экран больше либо равен то код не сработает
+  } else if (window.innerWidth >= 768 && swiper) {
+    //если экран больше либо равен то код не сработает
     swiper.destroy(true, true); // уничтожаем Swiper
     swiper = null;
   }
@@ -27,10 +30,30 @@ hideButton.addEventListener("click", hide);
 
 function show(event) {
   hiddenElements.style.height = "auto";
+  hideButton.style.height = "auto";
+  showButton.style.overflow = "visible";
+  showButton.style.opacity = "0";
 }
-
 function hide(event) {
   hiddenElements.style.height = "0px";
+  hideButton.style.height = "0px";
+  showButton.style.overflow = "hidden";
+  showButton.style.opacity = "1";
+};
+
+const desktopElementsShow = document.querySelectorAll(
+  ".desktop-version-elements",
+);
+
+function toggleDesktopElements() {
+  desktopElementsShow.forEach((el) => {
+    if (window.innerWidth > 1119) {
+      el.style.display = "block";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
 
-
+toggleDesktopElements();
+window.addEventListener("resize", toggleDesktopElements);
